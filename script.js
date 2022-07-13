0dd0f6c572e5aee9ae99371be8231ecf5e46d988
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-13 16:43:31 +0100

    run and move method

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridContainer = document.querySelector(".grid-container")
+    const scoreVal = document.querySelector(".gui__score")
+    const startBtn = document.querySelector(".gui__start-btn")
 
-const gridContainer = document.querySelector(".grid-container")
-
-function makeRows(rows, cols) {
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      cell.innerText = (c + 1);
-      gridContainer.appendChild(cell).className = "grid-piece";
+    // Method for creating the basic grid system
+    function makeRows(rows, cols) {
+        gridContainer.style.setProperty('--grid-rows', rows);
+        gridContainer.style.setProperty('--grid-cols', cols);
+        for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        // gridContainer.appendChild(cell).className = "grid-piece";
+        gridContainer.appendChild(cell).id = i;
+        };
     };
-};
-  
-makeRows(20, 20);
+    makeRows(20, 20);
+
+    const gridPieces = document.querySelectorAll('.grid-container div')
+    
+    // Declaring all the main variables
+    const width = 20;
+    let currentSnake = [ 208, 209, 210];
+    let direction = 1;
+    let score = 0;
+    let interval = 0;
+    let timeBetween = 0;
+    let currentIndex = 0;
+
+    // Resets all the variables to the original
+    const runGame = () => {
+        currentSnake.forEach(index => gridPieces[index].classList.remove('grid-piece--snake'));
+        console.log("removed");
+        clearInterval(interval)
+        score = 0;
+        direction = 1;
+        scoreVal.innerHTML = `Score: ${score}`;
+        // Time between frames in ms
+        timeBetween = 200;
+        currentSnake = [ 208, 209, 210];
+        currentIndex = 0;
+        currentSnake.forEach(index => gridPieces[index].classList.add('grid-piece--snake'))
+        interval = 0;
+    }
+
+    //snake control with key strokes
+    const snakeControl = (event) => {
+        if(event.keyCode === 39) direction = 1;
+        else if (event.keyCode === 38) direction = - width;
+        else if (event.keyCode === 37) direction = - 1;
+        else if (event.keyCode === 40) direction = + width;
+    }
+
+    document.addEventListener('keydown', snakeControl)
+    startBtn.addEventListener('click', runGame)
+})
 
 //const gridPieces = document.querySelector(".grid-container")
