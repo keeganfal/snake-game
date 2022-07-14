713f2b9e5712c2ef0b2d9afb2a9345316c68aa09
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-14 01:36:55 +0100

    random food & snake cant go backwards

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const scoreVal = document.querySelector(".gui__score")
 const startBtn = document.querySelector(".gui__start-btn")
 
 // Method for creating the basic grid system
-function makeRows(rows, cols) {
+function makeRows(rows, column) {
     gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
-    for (let i = 0; i < (rows * cols); i++) {
+    gridContainer.style.setProperty('--grid-column', column);
+    for (let i = -1; i < (rows * column) - 1; i++) {
     let cell = document.createElement("div");
     cell.innerText = (i + 1);
     // gridContainer.appendChild(cell).className = "grid-piece";
@@ -25,12 +25,12 @@ let score = 0;
 // grid piece loc of the snake
 let currentSnake = [ 210, 209, 208];
 let snakeDirection = 1;
-let currentIndex = 0;
+
+let foodLoc = 0;
 
 // runs method with a time interval
 let timeBetween = 0;
 let interval = 0;
-
 
 // Resets all the variables to the original
 const runGame = () => {
@@ -42,17 +42,26 @@ const runGame = () => {
     // Time between frames in ms
     timeBetween = 200;
     currentSnake = [ 210, 209, 208];
-    currentIndex = 0;
-    currentSnake.forEach(index => gridPieces[index].classList.add('grid-piece--snake'))
+
+    currentSnake.forEach(index => gridPieces[index].classList.add('grid-piece--snake'));
+    randomFood();
     interval = setInterval(ifCollides, timeBetween);
 }
 
+//snake control with key strokes
+const snakeControl = (event) => {
+    if(event.keyCode === 39 && snakeDirection != -1) snakeDirection = 1;
+    else if (event.keyCode === 38 && snakeDirection != width) snakeDirection = - width;
+    else if (event.keyCode === 37 && snakeDirection != 1) snakeDirection = - 1;
+    else if (event.keyCode === 40 && snakeDirection != -width) snakeDirection = + width;
+}
+
 const ifCollides = () => {
-    if ((currentSnake[0] + width >= (width*width) && snakeDirection === width) ||
+    if (gridPieces[currentSnake[0] + snakeDirection].classList.contains('grid-piece--snake') || 
+    (currentSnake[0] + width >= (width*width) && snakeDirection === width) ||
     (currentSnake[0] % width === (width - 1) && snakeDirection === 1) ||
     (currentSnake[0] % width === 0 && snakeDirection === - 1) ||
-    (currentSnake[0] - width < 0 && snakeDirection === - width) ||
-    gridPieces[currentSnake[0] + snakeDirection].classList.contains('grid-piece--snake')
+    (currentSnake[0] - width < 0 && snakeDirection === - width)
     ) {
         alert("GAME OVER")
         return clearInterval(interval);
@@ -63,14 +72,16 @@ const ifCollides = () => {
     gridPieces[lastItem].classList.remove('grid-piece--snake')
     currentSnake.unshift(currentSnake[0] + snakeDirection) 
     gridPieces[currentSnake[0]].classList.add('grid-piece--snake')
+}
+
+//adds random food
+const randomFood = () => {
+    foodLoc = Math.floor(Math.random() * gridPieces.length);
+    for (i in gridPieces){
+        if (!gridPieces[foodLoc].classList.contains('grid-piece--snake')) {
+            gridPieces[foodLoc].classList.add('grid-piece--food');
+        }
     }
-    
-//snake control with key strokes
-const snakeControl = (event) => {
-    if(event.keyCode === 39) snakeDirection = 1;
-    else if (event.keyCode === 38) snakeDirection = - width;
-    else if (event.keyCode === 37) snakeDirection = - 1;
-    else if (event.keyCode === 40) snakeDirection = + width;
 }
 
 document.addEventListener('keydown', snakeControl)
